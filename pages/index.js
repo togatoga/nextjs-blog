ac879953954e7b7a0fa13334e11b6143ba7b0ebc
togatoga <togasakitogatoga@gmail.com>
2022-01-17 18:48:07 +0900

    Update

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
-export default function Home() {
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import { getSortedPostsData } from "../lib/posts";
+import utilStyles from "../styles/utils.module.css";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -14,6 +24,20 @@ export default function Home() {
           <a href="https://nextjs.org/learn">Our Next.js tutorial</a>
         </p>
       </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
-  )
+  );
 }
